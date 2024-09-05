fec637db39121c6de469b44687dd8e08cda9a8ad
chris <christian.schuetz@bluewin.ch>
2024-09-05 22:42:27 +0200

    plotted some figures

--- testing.R
+++ testing.R
@@ -1,3 +1,14 @@
 library(tidyverse)
 
-mtcars |> mutate(names = rownames(mtcars), .before =mpg ) |> as.tibble()
+rm(list = ls())
+
+df <- mtcars |> mutate(name = rownames(mtcars), .before =mpg ) |> 
+  as.tibble() |> 
+  pivot_longer(cols = mpg:carb,names_to = "parameter", values_to = "value")
+
+
+df |> ggplot(mapping = aes(x = parameter, y = value)) +
+  geom_point()
+
+df |> ggplot(mapping = aes(x = parameter, y = value)) +
+  geom_boxplot()
